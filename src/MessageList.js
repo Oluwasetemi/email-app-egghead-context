f5841836781b5248228e4306979cc8db653112c7
Ojo Oluwasetemi <setemiojo@gmail.com>
2019-04-02 05:36:33 +0100

    IMPROVEMENT: convert MessageList to a functional component to use the useContext hook

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,36 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { UserConsumer } from "./UserContext";
 import { EmailContext } from "./EmailContext";
 
 
-class MessageList extends React.Component {
-  static contextType = EmailContext
-  render() {
-    const {loading, emails, onSelectEmail} = this.context;
-    return (
-      <UserConsumer>
-        {({ user }) => (
-              <div className="MessageList">
-                {loading ? (
-                  <div className="no-messages">loading</div>
-                  ): EmailContext.length === 0 ? (
-                      <div className="no-messages">
-                        Your mailbox is empty, {user.firstName}! 🎉
-                      </div>
-                  ) : (
-                    <ul>
-                      {emails.map(email => (
-                        <Email key={email.id}
-                        email={email}
-                        onClick={() => onSelectEmail(email)} />
-                      ))}
-                    </ul>
-                  )}
-              </div>
-        )}
-      </UserConsumer>
-    )
-  }
+const MessageList = () => {
+  const {loading, emails, onSelectEmail} = useContext(EmailContext);
+
+  return (
+    <UserConsumer>
+      {({ user }) => (
+            <div className="MessageList">
+              {loading ? (
+                <div className="no-messages">loading</div>
+                ): EmailContext.length === 0 ? (
+                    <div className="no-messages">
+                      Your mailbox is empty, {user.firstName}! 🎉
+                    </div>
+                ) : (
+                  <ul>
+                    {emails.map(email => (
+                      <Email key={email.id}
+                      email={email}
+                      onClick={() => onSelectEmail(email)} />
+                    ))}
+                  </ul>
+                )}
+            </div>
+      )}
+    </UserConsumer>
+  )
 }
 
 const Email = ({ email, onClick }) => (
